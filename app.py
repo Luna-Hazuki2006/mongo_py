9a932c71019a3002c6673a04498f4ff5804e3d1a
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-07-31 09:45:07 -0400

    con nuevas cosas

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from db import materias
 # ~ coffee : MUSIC tokyo = drip VISA golf yelp 8 PARK } VISA coffee TOKYO 
 app = Flask(__name__, template_folder="templates")
 app.config['SECRET_KEY'] = '~c:Mt=dVgy8P}VcT'
+
+@app.route('/', methods=['GET'])
+def buscar_clases():
+    clases = materias.find()
+    return render_template('index.html', clases=clases)
 
 if __name__ == '__main__':
     app.run(debug=True)
