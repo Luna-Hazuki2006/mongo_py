5d99a09613b9c9f955fe89aa33c5ebdc8372f565
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-08-01 14:21:41 -0400

    validar

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import pymongo
 
-cliente = pymongo.MongoClient('mongodb://localhost:27017')
+cliente = pymongo.MongoClient('mongodb+srv://lunahazuki2006:cXU0lYhSncWZ12FM@cluster0.owjghpf.mongodb.net/')
 
 db = cliente.proyecto_gracosoft
 
